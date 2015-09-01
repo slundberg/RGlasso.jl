5aa30bc90d44c8b7f93c4ea74358a330c5a8ecc4
Scott Lundberg <scottmlundberg@gmail.com>
2015-08-31 18:44:17 -0700

    Allow custom R command

--- src/RGlasso.jl
+++ src/RGlasso.jl
@@ -2,7 +2,7 @@ module RGlasso
 
 export glasso
 
-function glasso(S::Matrix, rho::Float64)
+function glasso(S::Matrix, rho::Float64, rcmd="R")
     writedlm("/tmp/RGlasso_tmp_matrix.txt", S)
 
     # start R and tell it to run glasso with a given rho value
@@ -10,7 +10,7 @@ function glasso(S::Matrix, rho::Float64)
             "S = as.matrix(read.table('/tmp/RGlasso_tmp_matrix.txt', header=FALSE));" *
             "out = glasso(S, $rho);" *
             "write.table(out\$wi, '/tmp/RGlasso_tmp_matrix.txt', row.names=FALSE, col.names=FALSE)"
-    run(pipe(`R -q -e $rcode`, stdout=DevNull))
+    run(pipe(`$rcmd -q -e $rcode`, stdout=DevNull))
 
     w = readdlm("/tmp/RGlasso_tmp_matrix.txt")
     rm("/tmp/RGlasso_tmp_matrix.txt")
